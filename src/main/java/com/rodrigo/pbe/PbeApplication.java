e2be7972dadd29e64f6f85d12cfff32b97bbbe58
Rodrigopca42 <rodrigopca426281@gmail.com>
2023-04-08 15:19:58 -0300

    Projeto criado

---
+++ src/main/java/com/rodrigo/pbe/PbeApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigo.pbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PbeApplication.class, args);
+	}
+
+}
